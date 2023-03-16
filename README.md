faee2a01dafec588d793b7314429bfa6ae2ec028
joegor85 <joe.gor85@gmail.com>
2023-03-16 12:36:47 -0500

    Fully functional, just styling at this point

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Practice pulling data from API's and building an interface for it.
 
 I wanted to do a jobsearch project but could not find a good user-friendly API for it. Decided to use an xkcd comic API instead.
+
+This just displays 20 random comic titles and allows you to click to view each one. If you find one you like, you can enter the comic number at the bottom and save it to the favorites part in the bottom section.
--- app.js
+++ app.js
@@ -25,41 +25,99 @@ fetch.addEventListener("click", (e) => {
 });
 
 let randomComics = [];
+let comicTitleContainer = document.getElementById("comicTitleContainer");
+let comicContainer = document.getElementById("comicContainer");
 function fetchRandom() {
-  let comicTitleContainer = document.getElementById("comicTitleContainer");
-  let comicContainer = document.getElementById("comicContainer");
   for (let i = 0; i < numbersArray.length; i++) {
     $.get(`https://xkcd.vercel.app/?comic=${numbersArray[i]}`, (data) => {
       randomComics.push(data);
       console.log(data.title);
-      let eachComic = document.createElement("img");
-      eachComic.src = data.img;
       let eachTitle = document.createElement("div");
       eachTitle.innerText = data.title;
       eachTitle.classList = "title";
+      let eachComic = document.createElement("img");
+      eachComic.src = data.img;
+      let eachComicData = document.createElement("div");
+      eachComicData.classList = "data";
+      eachComicData.innerText = `This is comic #${
+        data.num
+      } and it is from ${convertToMonth(data.month)} ${
+        data.year
+      }. Description/writer's comment:\n\n${data.alt}`;
       comicTitleContainer.append(eachTitle);
       eachTitle.addEventListener("click", (e) => {
+        let previousSelected = $(".selected");
+        console.log(previousSelected);
+        previousSelected.removeClass("selected");
+        //add classlist to style the selected title
+        eachTitle.classList.add("selected");
         //remove old comic (if any)
         $("#comicContainer").children().remove();
         //add clicked comic image to div "comicContainer"
         comicContainer.append(eachComic);
+        comicContainer.append(eachComicData);
       });
     });
   }
   console.log(randomComics);
 }
 
-function deleteChild() {
-    var e = document.querySelector("ul");
-    
-    //e.firstElementChild can be used.
-    var child = e.lastElementChild; 
-    while (child) {
-        e.removeChild(child);
-        child = e.lastElementChild;
-    }
+function convertToMonth(month) {
+  if (month == 1) {
+    return "January";
+  } else if (month == 2) {
+    return "February";
+  } else if (month == 3) {
+    return "March";
+  } else if (month == 4) {
+    return "April";
+  } else if (month == 5) {
+    return "May";
+  } else if (month == 6) {
+    return "June";
+  } else if (month == 7) {
+    return "July";
+  } else if (month == 8) {
+    return "August";
+  } else if (month == 9) {
+    return "September";
+  } else if (month == 10) {
+    return "October";
+  } else if (month == 11) {
+    return "November";
+  } else {
+    return "December";
+  }
 }
-var btn = document.getElementById(
-"btn").onclick = function() {
-    deleteChild();
+
+// On user input, pull the comic from the API and display in favorites.
+const favButton = document.querySelector(".favButton");
+let userInput = document.querySelector("input");
+favButton.addEventListener("click", (e) => {
+  // Run a function that grabs the favorite comic and adds it to the favorite container
+  getUserFavorite();
+});
+
+//define a function to grab user's favorite
+function getUserFavorite() {
+  //let favComicData = [];
+  console.log(userInput.value);
+  let eachFav = document.createElement("div");
+  eachFav.classList = "eachFav";
+  $.get(`https://xkcd.vercel.app/?comic=${userInput.value}`, (data) => {
+    let favTitle = document.createElement("div");
+    favTitle.innerText = data.title;
+    favTitle.classList = "favTitle";
+    let favComicImage = document.createElement("img");
+    favComicImage.classList = "favComic";
+    favComicImage.src = data.img;
+    let favDesc = document.createElement("div");
+    favDesc.innerText = data.alt;
+    favDesc.classList = "favDesc";
+    eachFav.append(favTitle);
+    eachFav.append(favComicImage);
+    eachFav.append(favDesc);
+    let favoriteContainer = document.getElementById("favoriteContainer");
+    favoriteContainer.append(eachFav);
+  });
 }
