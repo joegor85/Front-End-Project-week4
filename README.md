ae89a6568b0c2bbc752aaa157a9a349cf3bebb4b
joegor85 <joe.gor85@gmail.com>
2023-03-16 21:14:00 -0500

    Project Complete

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Front-End-Project-week4
-Practice pulling data from API's and building an interface for it.
 
-I wanted to do a jobsearch project but could not find a good user-friendly API for it. Decided to use an xkcd comic API instead.
+## Purpose:
 
-This just displays 20 random comic titles and allows you to click to view each one. If you find one you like, you can enter the comic number at the bottom and save it to the favorites part in the bottom section.
+The goal of this project was to practice pulling data from API's and building an interface for it with some interactivity.
+
+## Library used:
+
+\*jQuery
+
+## API used:
+
+\*xkcd comics (https://xkcd.vercel.app/)
+
+## What this application does:
+
+The purpose of this page is for a user (you) to sift through randomly generated xkcd comics for entertainment purposes, select ones that you like, and add them to a list of favorites that you can then view at the bottom as well as export those image links for bookmarking or other purposes.
+
+To begin, just click on the top button ("Fetch random xkd comics!!!"). This will display a list of 20 titles to choose from.
+
+Click on a title to get a preview of that particular comic with some other information about ithe comic and when it was made. If you like that particular one, you can paste its number (found in the description below the comic) into the "Add to favorites" input box. Click "Add to Favorites" to add it to the display at the bottom.
+
+Once you have a collection of your favorites, click the "Collect links!" button to view the comic number and it's url. You can bookmark these URL's for later use on your device or you can share with a friend if you wish.
+
+### Other notes
+
+I originally wanted to do a jobsearch project but could not find a good user-friendly API for it. Most job posting API's required tokens and such. I decided on an xkcd API and even ran into CORS errors that I could not solve with the first one. I was able to find another one that worked well though.
--- app.js
+++ app.js
@@ -3,35 +3,46 @@
 //   console.log(data);
 // });
 
+// Create function to generate 20 random numbers to use in finding comics
 let numbersArray = [];
 function randomNumbers() {
   for (let i = 0; i < 20; i++) {
     //console.log(Math.floor(Math.random() * (2500 - 1) + 1));
-    let number = Math.floor(Math.random() * (2500 - 1) + 1);
+    let number = Math.floor(Math.random() * (2750 - 1) + 1);
     numbersArray.push(number);
   }
 }
-randomNumbers();
-console.log(numbersArray);
+
+//------------------------FETCH COMICS---------------------------------//
 
 // On button click, generate 20 random comics and store them in memory. Then display keywords found in those comics.
 const fetch = document.querySelector(".fetch");
 fetch.addEventListener("click", (e) => {
+  numbersArray = [];
+  randomNumbers();
+  console.log(numbersArray);
   // console.log(e.target);
-  // Run a function that grabs random data
+  // Remove any existing titles if button was clicked previously.
   $("#comicTitleContainer").children().remove();
   $("#comicContainer").children().remove();
+  // Run a function that grabs random data
   fetchRandom();
 });
 
+// Set up some variables that we will need
 let randomComics = [];
 let comicTitleContainer = document.getElementById("comicTitleContainer");
 let comicContainer = document.getElementById("comicContainer");
+let comicDataContainer = document.getElementById("comicDataContainer");
+// Define the function which requests the data through the API, compiles it, and places it on the page
 function fetchRandom() {
   for (let i = 0; i < numbersArray.length; i++) {
+    // Access the data
     $.get(`https://xkcd.vercel.app/?comic=${numbersArray[i]}`, (data) => {
+      // Add it to our array of randomComics
       randomComics.push(data);
       console.log(data.title);
+      // Create the elements needed for the page
       let eachTitle = document.createElement("div");
       eachTitle.innerText = data.title;
       eachTitle.classList = "title";
@@ -45,23 +56,28 @@ function fetchRandom() {
         data.year
       }. Description/writer's comment:\n\n${data.alt}`;
       comicTitleContainer.append(eachTitle);
+      // Add an event listener to each of the titles to make them clickable
       eachTitle.addEventListener("click", (e) => {
+        // Remove the styling of the previously selected title
         let previousSelected = $(".selected");
         console.log(previousSelected);
         previousSelected.removeClass("selected");
-        //add classlist to style the selected title
+        // Add classlist to style the selected title
         eachTitle.classList.add("selected");
-        //remove old comic (if any)
+        // Remove old comic (if any)
         $("#comicContainer").children().remove();
-        //add clicked comic image to div "comicContainer"
+        $("#comicDataContainer").children().remove();
+        // Add clicked comic image to div "comicContainer"
         comicContainer.append(eachComic);
-        comicContainer.append(eachComicData);
+        comicDataContainer.append(eachComicData);
       });
     });
   }
+  // View the comics data in the console if needed
   console.log(randomComics);
 }
 
+// Define convertToMonth function for use in above function
 function convertToMonth(month) {
   if (month == 1) {
     return "January";
@@ -89,7 +105,9 @@ function convertToMonth(month) {
     return "December";
   }
 }
-
+//------------------------FAVORITES--------------------------//
+// Make a container array to store our favorite data
+let userFavorites = [];
 // On user input, pull the comic from the API and display in favorites.
 const favButton = document.querySelector(".favButton");
 let userInput = document.querySelector("input");
@@ -98,26 +116,50 @@ favButton.addEventListener("click", (e) => {
   getUserFavorite();
 });
 
-//define a function to grab user's favorite
+// Define the function to grab user's favorite
 function getUserFavorite() {
-  //let favComicData = [];
   console.log(userInput.value);
-  let eachFav = document.createElement("div");
-  eachFav.classList = "eachFav";
-  $.get(`https://xkcd.vercel.app/?comic=${userInput.value}`, (data) => {
-    let favTitle = document.createElement("div");
-    favTitle.innerText = data.title;
-    favTitle.classList = "favTitle";
-    let favComicImage = document.createElement("img");
-    favComicImage.classList = "favComic";
-    favComicImage.src = data.img;
-    let favDesc = document.createElement("div");
-    favDesc.innerText = data.alt;
-    favDesc.classList = "favDesc";
-    eachFav.append(favTitle);
-    eachFav.append(favComicImage);
-    eachFav.append(favDesc);
-    let favoriteContainer = document.getElementById("favoriteContainer");
-    favoriteContainer.append(eachFav);
-  });
+  // Since there are only 2750 comics(as of now), best to limit the input with an alert message
+  if (userInput.value > 2750) {
+    alert(
+      "As of the creation of this website, there are only 2750 xkcd comics..."
+    );
+  } else {
+    // Create the HTML elements we need and put the fav data into them
+    let eachFav = document.createElement("div");
+    eachFav.classList = "eachFav";
+    $.get(`https://xkcd.vercel.app/?comic=${userInput.value}`, (data) => {
+      let favTitle = document.createElement("div");
+      favTitle.innerHTML =
+        data.title +
+        "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp(" +
+        convertToMonth(data.month) +
+        " " +
+        data.day +
+        ", " +
+        data.year +
+        ")";
+      favTitle.classList = "favTitle";
+      let favComicImage = document.createElement("img");
+      favComicImage.classList = "favComic";
+      favComicImage.src = data.img;
+      let favDesc = document.createElement("div");
+      favDesc.innerText = data.alt;
+      favDesc.classList = "favDesc";
+      eachFav.append(favTitle);
+      eachFav.append(favComicImage);
+      eachFav.append(favDesc);
+      let favoriteContainer = document.getElementById("favoriteContainer");
+      favoriteContainer.append(eachFav);
+      // Collect # and URL data on favorites for display later
+      let combined = "#" + data.num + " " + data.img;
+      userFavorites.push(combined);
+    });
+  }
 }
+
+// Add an event listener so when user clicks on "get links" button it provides them with direct links to their favorites
+const urlButton = document.querySelector(".urlButton");
+urlButton.addEventListener("click", (e) => {
+  alert(userFavorites.join("                      "));
+});
